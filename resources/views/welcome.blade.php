7e66197a700ee6ba3a5b181482be77d18026e45e
risqid <risqidharmawan@gmail.com>
2026-06-20 08:45:06 +0700

    Test: push dan tunggu CI hijau atau selesai -> baru buat tag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
                             <a href="https://cloud.laravel.com" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                                Bugfix nginx 502 v1.0.1
+                                Test: push dan tunggu CI hijau atau selesai -> baru buat tag v1.0.2
                             </a>
                         </li>
                     </ul>
